209de44d3de8c107745b3c57f2deb11cd9540594
SmachuloTetiana <tanichk@ukr.net>
2019-04-08 13:17:40 +0300

    commit changes

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 
-Injectable()
+@Injectable()
 export class AuthService {
     constructor(
         private af: AngularFireAuth
